230969287f64ba34b5e34855124000e886dcd0ed
Nikita Almanov <131481562+nikkeyl@users.noreply.github.com>
2024-02-20 17:57:55 +0300

    test(rule): optimize

--- specs/creators/rule.spec.js
+++ specs/creators/rule.spec.js
@@ -4,39 +4,39 @@ import { describe, it } from 'mocha';
 import createRule from '../../src/creators/rule.js';
 
 describe('Create Rule', () => {
-  it('create an object with args (selector: String, [::pseudo-element])', () => {
-    const rule = createRule('::pseudo-element');
+  let rule;
 
-    expect(rule).to.have.property('selector', '&::pseudo-element');
+  beforeEach(() => {
+    rule = createRule;
+  });
+
+  it('create an object with args (selector: String, [::pseudo-element])', () => {
+    expect(rule('::pseudo-element')).to.have.property(
+      'selector',
+      '&::pseudo-element',
+    );
   });
 
   it('create an object with args (selector: String, [:pseudo-class])', () => {
-    const rule = createRule(':pseudo-class');
-
-    expect(rule).to.have.property('selector', '&:pseudo-class');
+    expect(rule(':pseudo-class')).to.have.property('selector', '&:pseudo-class');
   });
 
   it('create an object with args (selector: RegExp, [\\[[^\\[\\]]+\\]])', () => {
-    const rule = createRule('\\[[^\\[\\]]+\\]');
-
-    expect(rule).to.have.property('selector', '&\\[[^\\[\\]]+\\]');
+    expect(rule('\\[[^\\[\\]]+\\]')).to.have.property(
+      'selector',
+      '&\\[[^\\[\\]]+\\]',
+    );
   });
 
   it('create an object with args (selector: RegExp, [\\..+])', () => {
-    const rule = createRule('\\..+');
-
-    expect(rule).to.have.property('selector', '&\\..+');
+    expect(rule('\\..+')).to.have.property('selector', '&\\..+');
   });
 
   it('create an object with args (selector: RegExp, [--.+])', () => {
-    const rule = createRule('--.+');
-
-    expect(rule).to.have.property('selector', '&--.+');
+    expect(rule('--.+')).to.have.property('selector', '&--.+');
   });
 
   it('create an object with args (selector: RegExp, [?.* &])', () => {
-    const rule = createRule('?.* &');
-
-    expect(rule).to.have.property('selector', '&?.* &');
+    expect(rule('?.* &')).to.have.property('selector', '&?.* &');
   });
 });
