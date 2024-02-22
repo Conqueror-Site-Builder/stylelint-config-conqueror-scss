c4054e6ab66e8458e7e33e1678f367f90f646bd6
Nikita Almanov <131481562+nikkeyl@users.noreply.github.com>
2024-02-22 21:31:05 +0300

    feat: add `rollup-plugin-dts`

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,26 @@
 import { defineConfig } from 'rollup';
+
+import { dts } from 'rollup-plugin-dts';
 import { minify } from 'rollup-plugin-esbuild';
+
 import typescript from '@rollup/plugin-typescript';
 
-export default defineConfig({
-  plugins: [typescript(), minify()],
-  input: 'src/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'es',
+const declarationFile = 'index.d.ts';
+
+export default defineConfig([
+  {
+    plugins: [typescript(), minify()],
+    input: 'src/index.ts',
+    output: {
+      file: 'index.js',
+      format: 'es',
+    },
   },
-});
+  {
+    plugins: [dts()],
+    input: `dist/src/${declarationFile}`,
+    output: {
+      file: declarationFile,
+    },
+  },
+]);
