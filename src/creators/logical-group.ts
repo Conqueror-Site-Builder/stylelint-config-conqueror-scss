9eb72eae7a5bc22f53c678be17d22502a3394c32
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-02-26 22:23:35 +0300

    refactor(logical-group): add correct type

--- src/creators/logical-group.ts
+++ src/creators/logical-group.ts
@@ -3,13 +3,13 @@
  * with specified properties and formatting rules.
  * @param {string} groupName - The `groupName` parameter is a string that
  * represents the name of the logical group being created.
- * @param {any[]} properties - The `properties` parameter in the
+ * @param {string[]} properties - The `properties` parameter in the
  * `createLogicalGroup` function is an array that contains the properties
  * or items that you want to group together under a logical group
  * with the specified `groupName`. These properties can be of any type or
  * structure, depending on your specific use case.
  */
-export default async (groupName: string, properties: any[]) => ({
+export default async (groupName: string, properties: string[]) => ({
   groupName,
   properties,
   emptyLineBefore: 'always',
