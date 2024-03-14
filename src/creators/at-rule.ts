d8ac46def8637d069ec2dbeedd10181a7d2fc77b
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-03-14 23:57:41 +0300

    fix(regex): change name

--- src/creators/at-rule.ts
+++ src/creators/at-rule.ts
@@ -1,8 +1,10 @@
 /**
  * The asynchronous `createAtRule` function in TypeScript creates an object
  * representing an at-rule with a specified name and block presence.
+ *
  * @param {string} name - The `name` parameter is a string that represents
  * the name of the at-rule being created.
+ *
  * @param {boolean} hasBlock - The `hasBlock` parameter in the `createAtRule`
  * function is a boolean value that indicates whether the at-rule being created
  * will have a block of nested rules or declarations inside it. If `hasBlock`
