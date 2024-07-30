738d2f77a394948de9cfaac83634abca0591604b
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-07-30 16:13:52 +0300

    spec: rename

--- specs/patterns/patterns.spec.ts
+++ specs/patterns/patterns.spec.ts
@@ -1,13 +1,13 @@
 import { describe, expect, test as spec } from 'vitest';
 
-import { regex } from '#shared';
+import { patterns } from '#shared';
 
 describe('Nested Selectors Pattern', () => {
-  const NESTED_ATTRIBUTE = regex.nested.ATTRIBUTE_PATTERN;
-  const NESTED_CLASS = regex.nested.CLASS_PATTERN;
-  const NESTED_MODIFIER = regex.nested.MODIFIER_PATTERN;
-  const CHILD_ATTRIBUTE = regex.child.ATTRIBUTE_PATTERN;
-  const CHILD_CLASS = regex.child.CLASS_PATTERN;
+  const NESTED_ATTRIBUTE = patterns.nested.ATTRIBUTE_PATTERN;
+  const NESTED_CLASS = patterns.nested.CLASS_PATTERN;
+  const NESTED_MODIFIER = patterns.nested.MODIFIER_PATTERN;
+  const CHILD_ATTRIBUTE = patterns.child.ATTRIBUTE_PATTERN;
+  const CHILD_CLASS = patterns.child.CLASS_PATTERN;
 
   spec('nested (&[attribute)', async () => {
     expect('&[attribute]').match(new RegExp(`&${NESTED_ATTRIBUTE}`));
